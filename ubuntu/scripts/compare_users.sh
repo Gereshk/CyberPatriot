c63916cbc830bdee6903cb980aee34116d44713c
Gereshk <117225564+Gereshk@users.noreply.github.com>
2022-12-16 12:27:09 -0800

    Update compare_users.sh

--- ubuntu/scripts/compare_users.sh
+++ ubuntu/scripts/compare_users.sh
@@ -1,9 +1,15 @@
-#!/bin/bash
 
+# Prompt the user for the name of the file containing the list of users
 read -p "Enter the name of the file containing the list of users: " user_file
 
 # Remove any users that are not in the file
-for user in $(cut -d: -f1/etc/passwd); do
+for user in $(cut -d: -f1 /etc/passwd); do
+  # Skip users with a UID below 1000 (system users)
+  uid=$(id -u "$user")
+  if [ "$uid" -lt 1000 ]; then
+    continue
+  fi
+
   if ! grep -q "^$user$" "$user_file"; then
     userdel "$user"
   fi

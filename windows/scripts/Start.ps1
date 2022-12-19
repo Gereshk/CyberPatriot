d5688c9f255637bf9185232ba56aa555c460cee3
Gereshk <117225564+Gereshk@users.noreply.github.com>
2022-12-19 10:37:33 -0800

    Update Start.ps1

--- windows/scripts/Start.ps1
+++ windows/scripts/Start.ps1
@@ -1,4 +1,3 @@
-:menu
 # Display menu
 Write-Host "What would you like to do?"
 Write-Host "1. Configure Windows Update"
@@ -6,6 +5,9 @@ Write-Host "2. Set password policy"
 Write-Host "3. Enable and update Windows Defender"
 Write-Host "4. Stop sharing shared folders and disable Autoplay"
 Write-Host "5. Require Ctrl+Alt+Del before logging in"
+Write-Host "6. Fix users"
+Write-Host "7. Fix admins"
+Write-Host "8. Fix Passwords"
 Write-Host "Enter the number of the task you want to perform, or 0 to exit:"
 $choice = Read-Host
 
@@ -32,10 +34,20 @@ switch ($choice)
         # Launch configure_cad.ps1 script
         Start-Process -FilePath .\configure_cad.ps1
     }
+    6 {
+        # Fix users
+        Start-Process -FilePath .\fix_users.ps1
+    }
+    7 {
+        # Fix admins
+        Start-Process -FilePath .\fix_admins.ps1
+    }
+    8 {
+        # Launch fix_passwords.ps1 script
+        Start-Process -FilePath .\fix_passwords.ps1
+    }
     0 {
         # Exit script
         exit
     }
 }
-
-#
